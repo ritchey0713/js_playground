938aeaae5ec15bffc26433a83dc6658dc6c3702a
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-12 23:47:44 -0400

    aadd logics

--- basics/advancedIF.js
+++ basics/advancedIF.js
@@ -1,8 +1,39 @@
 let isAccountLocked = false;
 let userRole = 'user'
 
+// if(isAccountLocked){
+//   console.log("sorry this account is locked!")
+// }else{
+//   console.log("This account is unlocked!")
+// }
+
+
 if(isAccountLocked){
   console.log("sorry this account is locked!")
+}else if (userRole === 'admin'){
+  console.log("welcome admin")
 }else{
-  console.log("This account is unlocked!")
+  console.log("welcome user!")
+}
+
+
+let temp = 110
+
+if(temp <= 32){
+  console.log("its freezing outside!")
+}else if(temp > 33 && temp < 90){
+  console.log("its a nice day!")
+}else{
+  console.log("its hot out here!")
+}
+
+let isGuestOneVegan = false
+let isGuestTwoVegan = false
+
+if(isGuestOneVegan && isGuestTwoVegan){
+  console.log("Vegan menu for both!")
+}else if(isGuestOneVegan || isGuestTwoVegan){
+console.log("We need one vegan menu")
+}else{
+  console.log("No vegan menus needed")
 }
