ace6d2b8fe18e64e532b6eebd3cd0d6c604f0d31
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-12 23:34:11 -0400

    add conversion

---
+++ basics/tempConversion.js
@@ -0,0 +1,10 @@
+let fahrenheit = 50 // 0 cel 273.15 kel
+
+//calculate celsius and store then print  (fahrenheit - 32) * (5/9)
+// calculate in kelvin store and print (fahrenheit + 459.67) * (5/9)
+
+let celsius = (fahrenheit - 32) * (5/9)
+console.log(celsius)
+
+let kelvin = (fahrenheit + 459.67) * (5/9)
+console.log(kelvin)
