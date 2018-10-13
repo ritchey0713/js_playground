53fd2c5dd2462d7cc8f7e0368168670f2e92c792
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-13 00:03:35 -0400

    add more logic

--- basics/advancedIF.js
+++ basics/advancedIF.js
@@ -1,21 +1,21 @@
-let isAccountLocked = false;
-let userRole = 'user'
+// let isAccountLocked = false;
+// let userRole = 'user'
+
+// // if(isAccountLocked){
+// //   console.log("sorry this account is locked!")
+// // }else{
+// //   console.log("This account is unlocked!")
+// // }
+
+
 
 // if(isAccountLocked){
 //   console.log("sorry this account is locked!")
+// }else if (userRole === 'admin'){
+//   console.log("welcome admin")
 // }else{
-//   console.log("This account is unlocked!")
+//   console.log("welcome user!")
 // }
-
-
-if(isAccountLocked){
-  console.log("sorry this account is locked!")
-}else if (userRole === 'admin'){
-  console.log("welcome admin")
-}else{
-  console.log("welcome user!")
-}
-
 
 let temp = 110
 
@@ -27,13 +27,13 @@ if(temp <= 32){
   console.log("its hot out here!")
 }
 
-let isGuestOneVegan = false
-let isGuestTwoVegan = false
+// let isGuestOneVegan = false
+// let isGuestTwoVegan = false
 
-if(isGuestOneVegan && isGuestTwoVegan){
-  console.log("Vegan menu for both!")
-}else if(isGuestOneVegan || isGuestTwoVegan){
-console.log("We need one vegan menu")
-}else{
-  console.log("No vegan menus needed")
-}
+// if(isGuestOneVegan && isGuestTwoVegan){
+//   console.log("Vegan menu for both!")
+// }else if(isGuestOneVegan || isGuestTwoVegan){
+// console.log("We need one vegan menu")
+// }else{
+//   console.log("No vegan menus needed")
+// }
