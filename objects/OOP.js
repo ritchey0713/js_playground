c70675c6a5f30bbb84c102291d92b39512545606
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-13 20:25:31 -0400

    add iterator to prototype

--- objects/OOP.js
+++ objects/OOP.js
@@ -1,16 +1,27 @@
-const Person = function(firstName, lastName, age){
+const Person = function(firstName, lastName, age, likes){
   this.firstName = firstName 
   this.lastName = lastName
   this.age = age
+  this.likes = likes
 }
 
 Person.prototype.getBio = function(){
-  return `${this.firstName} is ${this.age}`
+  let bio = `${this.firstName} is ${this.age}`
+    this.likes.forEach(like => {
+      bio += ` ${this.firstName} likes ${like}`
+    });
+  return bio
 }
 
-const me = new Person("John", "Smith", 46)
-me.firstName = "Dave"
-console.log(me)
+Person.prototype.setName = function(fullName){
+  const name = fullName.split(" ")
+  this.firstName = name[0]
+  this.lastName = name[1]
+}
 
-const person2 = new Person("Tony", "Turner", 19)
-console.log(person2)
+const me = new Person("John", "Smith", 46, ["games", "drifting", "shooting"])
+me.setName("Dave Smith")
+console.log(me.getBio())
+
+const person2 = new Person("Tony", "Turner", 19, ["Golfing", "Frisbee"])
+console.log(person2.getBio())
