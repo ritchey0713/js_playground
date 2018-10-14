354e85e0bfbab106b3cf8fab0126d9fd08d9394d
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-14 17:04:25 -0400

    add iterator

--- objects/notes.js
+++ objects/notes.js
@@ -4,5 +4,9 @@ const notes = ["note 1", "note 2", "note 3"]
 
 notes[2] = "this is a new note!!"
 
+notes.forEach(() => {
+  console.log("testing?")
+})
+
 console.log(notes.length)
 console.log(notes)
