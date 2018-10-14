9d7229c5d048e61b5b17b76c8723ae7d1f457471
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-14 19:34:51 -0400

    create note objects

--- objects/notes.js
+++ objects/notes.js
@@ -1,20 +1,35 @@
-const notes = ["note 1", "note 2", "note 3"]
+const notes = [{
+  title: "Vacation",
+  body: "I wanna go to Texas"
+},{
+  title: "Diet",
+  body: "Dont eat so much fast food"
+},{
+  title: "Algorithms",
+  body: "Practice more!"
+},{
+  title: "Land a Dev Job!",
+  body: "Network more and more!!!"
+}]
 
 //notes.splice(1, 1, "note 0")
 
-notes[2] = "this is a new note!!"
+// notes[2] = "this is a new note!!"
 
-notes.forEach(() => {
-  console.log("testing?")
-})
+// notes.forEach(() => {
+//   console.log("testing?")
+// })
 
 console.log(notes.length)
 console.log(notes)
 
-for(let i = 0; i < 3; i++ ){
- console.log(i)
-}
+// for(let i = 0; i < 3; i++ ){
+//   console.log(i)
+// }
 
-for(let i = 0; i < notes.length; i++){
-  console.log(notes[i])
-}
+// for(let i = 0; i < notes.length; i++){
+//   console.log(notes[i])
+// }
+
+
+console.log(notes.indexOf({}))
