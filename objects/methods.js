e8044d6bb4477e474458f10e2f6017c56772a052
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-14 11:25:42 -0400

    add object methods

--- objects/methods.js
+++ objects/methods.js
@@ -3,8 +3,26 @@ let restaurant = {
   guestCapacity: 75,
   guestCount: 0,
   checkAvailability: function(partySize){
-    console.log(partySize)
+    let seatsLeft = this.guestCapacity - this.guestCount
+    if(seatsLeft > partySize){
+    return true
+    }else{
+      return false
+    }
+  },
+  seatParty: function(partySize){
+    this.guestCount += partySize
+  },
+  removeParty: function(partySize){
+    this.guestCount -= partySize
   }
 }
 
-restaurant.checkAvailability(5)
+// let status = restaurant.checkAvailability(99)
+
+// console.log(status)
+
+restaurant.seatParty(72)
+console.log(restaurant.checkAvailability(4))
+restaurant.removeParty(5)
+console.log(restaurant.checkAvailability(4))
