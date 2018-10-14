06b28564ec52480167c60ccb9f2b8538b2ced2ce
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-14 16:49:07 -0400

    add number guesser

--- objects/numberMethods.js
+++ objects/numberMethods.js
@@ -1,1 +1,24 @@
 let num = 100.693
+
+console.log(num.toFixed(2))
+
+console.log(Math.round(num))
+console.log(Math.floor(num))
+console.log(Math.ceil(num))
+
+
+let min = 0
+let max = 1
+let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+console.log(randomNum)
+
+// range 1 -5 return true if guess is right 
+
+let makeGuess = (guess) => {
+  let min = 1
+  let max = 5
+  let answer = Math.floor(Math.random() * (max - min + 1)) + min
+  return answer === guess
+}
+
+console.log(makeGuess(3))
