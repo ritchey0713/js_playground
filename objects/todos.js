c432149d54d0717183e8cc7ab502e682af341d5c
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-16 12:59:45 -0400

    add delete method

--- objects/todos.js
+++ objects/todos.js
@@ -1,15 +1,27 @@
-const todos = ["finish car", 'get better at JS', "get a new job!", "buy more stuffs!"]
+const todos = [
+  {
+    text: "finish car",
+    completed: false}, 
+  {
+    text:'get better at JS',
+    completed: false}, 
+  {
+    text: "get a new job!",
+    completed: true}, 
+  { 
+    text:"buy more stuffs!",
+  completed:true }]
 
-console.log(`You have ${todos.length} things! to do`)
+// console.log(`You have ${todos.length} things! to do`)
 
-todos.splice(2,1)
-
-
-todos.push("find a new place to live!")
+// todos.splice(2,1)
 
 
-todos.shift()
-console.log(todos)
+// todos.push("find a new place to live!")
+
+
+// todos.shift()
+// console.log(todos)
 
 // todos.forEach((toDo, index) => {
 //   console.log(`${index + 1}. ${toDo}`)
@@ -19,3 +31,19 @@ console.log(todos)
 //   console.log(`${i + 1}. ${todos[i]} `)
 // }
 
+const searchTodos = (todos, text) => {
+  const deleteIndex = todos.findIndex((todo, index) => {
+    return todo.text.toLowerCase() === text.toLowerCase() 
+  })
+  if(deleteIndex > -1){
+    todos.splice(deleteIndex, 1)
+  }
+}
+
+
+searchTodos(todos, "finish car")
+// console.log(findTodo)
+
+//remove by index splice?
+
+console.log(todos)
