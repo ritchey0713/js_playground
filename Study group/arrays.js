e79ca632d989d34e979bdfc4d15126f5a9291768
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-17 22:30:53 -0400

    create SG

---
+++ Study group/arrays.js
@@ -0,0 +1,24 @@
+const notes = [{
+  title: "Vacation",
+  body: "Going on a trip to Spain"
+},{
+  title: "Wash the Car",
+  body: "Sunday will be nice, I should wash the car"
+},{
+  title: "Algorithms",
+  body: "Focus on the structure at hand"
+},{
+  title: "Clean the house",
+  body: "In-laws visiting soon, we should clean up!"
+}]
+
+const listNotes = notes.forEach((note, index) => {
+  console.log(`${index}.${note.title}: ${note.body}`)
+})
+
+listNotes
+
+
+// todos.forEach((toDo, index) => {
+//   console.log(`${index + 1}. ${toDo}`)
+// })
