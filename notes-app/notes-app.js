3ecac864ea6bea8f62c030ffe61cfefa67eef20f
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-23 19:33:14 -0400

    add notes

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,3 +1,24 @@
-const p = document.querySelector('p')
+const notes = [{
+  title: "Vacation",
+  body: "I wanna go to Texas"
+},{
+  title: "Diet",
+  body: "Dont eat so much fast food"
+},{
+  title: "Algorithms",
+  body: "Practice more!"
+},{
+  title: "land a Dev Job!",
+  body: "Network more and more!!!"
+}]
 
-console.log(p)
+const ps = document.querySelectorAll('p')
+
+ps.forEach(p => {
+  p.textContent = "******"
+});
+
+// add new element 
+const newPara = document.createElement('p')
+newPara.textContent  = 'this is a new element'
+document.querySelector('body').appendChild(newPara)
