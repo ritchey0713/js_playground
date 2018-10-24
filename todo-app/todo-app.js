45380661ba4c2915a4be1a02a7ad503c92daf351
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-24 19:42:45 -0400

    add changing filter

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -28,35 +28,30 @@ const renderTodos = function(todos, filters){
   const filteredtodos = todos.filter((todo) => {
     return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
   })
-  document.querySelector('#todos').innerHTML = ""
-  filteredtodos.forEach(todo => {
-    const todoele = document.createElement('p')
-    todoele.textContent = todo.text
-    document.querySelector('#todos').appendChild(todoele)
+    
+  
+  const toDosUnfinished =  filteredtodos.filter((todo) => {
+    return !todo.completed 
   })
+
+  document.querySelector('#todos').innerHTML = ""
+  
+  const toDoLeft = document.createElement("p")
+toDoLeft.textContent = `You have ${toDosUnfinished.length} left`
+document.querySelector('#todos').appendChild(toDoLeft)
+
+filteredtodos.forEach((todo) => {
+  const todoText = document.createElement('p')
+  todoText.textContent = todo.text
+  document.querySelector('#todos').appendChild(todoText)
+  }) 
 }
 
 renderTodos(todos, filters)
 
-// const toDosUnfinished =  todos.filter((todo) => {
-//     return todo.completed === false 
-//   })
-
- 
-
-// const toDoLeft = document.createElement("p")
-// toDoLeft.textContent = `You have ${toDosUnfinished.length} left`
-// document.querySelector('body').appendChild(toDoLeft)
-
-// todos.forEach((todo) => {
-//   const todoText = document.createElement('p')
-//   todoText.textContent = todo.text
-//   document.querySelector('body').appendChild(todoText)
-// }) 
-
-// document.querySelector('#add-todo').addEventListener("click", (e) => {
-//   console.log('clicked!')
-// })
+document.querySelector('#add-todo').addEventListener("click", (e) => {
+  console.log('clicked!')
+})
 
 document.querySelector('#add-todo-text').addEventListener('input', (e) => {
   filters.searchText = e.target.value
