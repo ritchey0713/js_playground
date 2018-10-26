1609a7f2f60dd33b1fc1128e8068f8d4d0c23f1b
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-26 13:04:53 -0400

    add stoarge event

--- notes-app/notes-edit.js
+++ notes-app/notes-edit.js
@@ -1,6 +1,10 @@
+const titleElement = document.querySelector('#note-title')
+const bodyElement = document.querySelector('#note-body')
+const removeButton = document.querySelector('#remove-note')
+
 const noteId = location.hash.substring(1)
-const notes = getSavedNotes()
-const note = notes.find((note) => {
+let notes = getSavedNotes()
+let note = notes.find((note) => {
   return note.id === noteId
 })
 
@@ -8,21 +12,38 @@ if(note === undefined){
   location.assign('/index.html')
 }
 
-const noteTitle = document.querySelector('#note-title').value = note.title
-const noteBody = document.querySelector('#note-body').value = note.body
+titleElement.value = note.title 
+bodyElement.value = note.body 
 
-const updateNoteTitle = document.querySelector('#note-title').addEventListener('input', (e) => {
+titleElement.addEventListener('input', (e) => {
   note.title = e.target.value
   saveNotes(notes)
 })
 
-const updateNoteBody = document.querySelector('#note-body').addEventListener('input', (e) => {
+bodyElement.addEventListener('input', (e) => {
   note.body = e.target.value
   saveNotes(notes)
 })
 
-document.querySelector('#remove-note').addEventListener('click', () => {
+removeButton.addEventListener('click', () => {
   removeNote(noteId)
   saveNotes(notes)
   location.assign('/index.html')
+})
+
+window.addEventListener('storage', (e) => {
+  if(e.key === 'notes'){
+    notes = JSON.parse(e.newValue)
+    let note = notes.find((note) => {
+      return note.id === noteId
+    })
+    
+    if(note === undefined){
+      location.assign('/index.html')
+    }
+    
+    titleElement.value = note.title 
+    bodyElement.value = note.body 
+    
+  }
 })
