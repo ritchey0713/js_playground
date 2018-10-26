3b6762956935f5c7983071522d835118f9884abd
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-25 22:19:03 -0400

    add reactive edit form

--- notes-app/notes-edit.js
+++ notes-app/notes-edit.js
@@ -8,5 +8,21 @@ if(note === undefined){
   location.assign('/index.html')
 }
 
-document.querySelector('#note-title').value = note.title
-document.querySelector('#note-body').value = note.body
+const noteTitle = document.querySelector('#note-title').value = note.title
+const noteBody = document.querySelector('#note-body').value = note.body
+
+const updateNoteTitle = document.querySelector('#note-title').addEventListener('input', (e) => {
+  note.title = e.target.value
+  saveNotes(notes)
+})
+
+const updateNoteBody = document.querySelector('#note-body').addEventListener('input', (e) => {
+  note.body = e.target.value
+  saveNotes(notes)
+})
+
+document.querySelector('#remove-note').addEventListener('click', () => {
+  removeNote(noteId)
+  saveNotes(notes)
+  location.assign('/index.html')
+})
