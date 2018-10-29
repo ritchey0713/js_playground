71f20f7593e8fd5a6af607602eb723eb35e4af70
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-29 19:23:39 -0400

    add subclass

--- objects/OOP.js
+++ objects/OOP.js
@@ -22,9 +22,25 @@ class Person {
 
 }
 
-const myPerson = new Person("John", "Smith", 28, ["Gaming", "Coding"] )
+class Employee extends Person {
+  constructor(firstName, lastName, age, position, likes){
+    super(firstName, lastName, age, likes)
+    this.position = position
+  }
+  getBio(){
+    return `${this.firstName} ${this.lastName} is a ${this.position}`    
+  }
+  getYearsLeft(){
+    return 65 - this.age
+  }
 
+
+}
+
+const myPerson = new Employee("John", "Smith", 28, "Developer", ["Gaming", "Coding"] )
+myPerson.setName("Tom Wells")
 console.log(myPerson.getBio())
+console.log(myPerson.getYearsLeft())
 
 // const Person = function(firstName, lastName, age, likes){
 //   this.firstName = firstName 
@@ -55,6 +71,6 @@ console.log(myPerson.getBio())
 // me.setName("Dave Smith")
 // console.log(me.getBio())
 
-// const person2 = new Person("Tony", "Turner", 19, ["Golfing", "Frisbee"])
+const person2 = new Person("Tony", "Turner", 19, ["Golfing", "Frisbee"])
 
-// console.log(person2.getBio())
+console.log(person2.getBio())
