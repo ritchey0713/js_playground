4c81bd4e3156f0da9fd56b4a0f99d8deadb33631
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-28 22:43:56 -0400

    add class

--- objects/OOP.js
+++ objects/OOP.js
@@ -1,3 +1,25 @@
+class PersonClass {
+  constructor(firstName, lastName, age, likes){
+    this.firstName = firstName 
+    this.lastName = lastName
+    this.age = age
+    this.likes = likes
+  }
+
+  getBio(){
+    let bio = `${this.firstName} is ${this.age}`
+    this.likes.forEach(like => {
+      bio += ` ${this.firstName} likes ${like}`
+    });
+  return bio
+  }
+
+}
+
+const myPerson = new PersonClass("John", "Smith", 28, ["Gaming", "Coding"] )
+
+console.log(myPerson.getBio())
+
 const Person = function(firstName, lastName, age, likes){
   this.firstName = firstName 
   this.lastName = lastName
