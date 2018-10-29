a5ec4ce3f615850e65beb31410289d688fbe0355
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-28 22:04:13 -0400

    add logs for status

--- hangman/app.js
+++ hangman/app.js
@@ -1,22 +1,25 @@
-//primitive value: string, null, number, boolean, undefined 
+gameOne = new Hangman("dog", 3)
 
-// obj: myObj -> Obj.proto -> null 
-// array: myArray -> Array.proto -> OBJ.proto -> null 
-//func: myfunc -> func.proto -> OBJ.proto -> null
-// string: myString -> string.proto -> obj.proto -> null
-// num: myNum -> num.proto -> obj.proto -> null
-// boolean: myBool -> bool.proto -> obj.proto -> null
+const gameWord = document.querySelector('#game-word')
+const guessesDOM = document.querySelector("#guesses-left")
 
-// const team = ["John", "Sarah"]
+gameWord.textContent = gameOne.getPuzzle()
+guessesDOM.textContent = gameOne.guessesLeft
+console.log(gameOne.status)
 
-// console.log(team)
 
-// const getScore = () => 1
+window.addEventListener('keypress', (e) => {
+const guess = String.fromCharCode(e.charCode)
+gameOne.makeGuess(guess)
 
-// console.log(getScore)
 
-// const product = 'computer'
-// console.log(product)
 
-// const otherProduct = new String('phone')
-// console.log(otherProduct)
+gameWord.textContent = gameOne.getPuzzle()
+guessesDOM.textContent = gameOne.guessesLeft
+console.log(gameOne.status)
+
+})
+
+
+
+
