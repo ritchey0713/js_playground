b538f1e543eec83ab85a359a43e14d698ce7fddb
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-30 10:59:28 -0400

    add xml request

--- hangman/app.js
+++ hangman/app.js
@@ -19,6 +19,15 @@ guessesDOM.textContent = gameOne.statusMessage
 
 })
 
+// make http request 
+const request = new XMLHttpRequest()
+request.addEventListener("readystatechange", (e) => {
+  if(e.target.readyState === 4){
+    const data = JSON.parse(e.target.responseText)
+      console.log(data)
+  }
+})
 
-
+request.open('GET', "http://puzzle.mead.io/puzzle")
+request.send()
 
