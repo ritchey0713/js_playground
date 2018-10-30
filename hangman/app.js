6cd7006b20ef7ef5fb48ac190c610625c06f93b6
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-30 13:42:51 -0400

    add api call

--- hangman/app.js
+++ hangman/app.js
@@ -20,14 +20,34 @@ guessesDOM.textContent = gameOne.statusMessage
 })
 
 // make http request 
+// const request = new XMLHttpRequest()
+// request.addEventListener("readystatechange", (e) => {
+//   if(e.target.readyState === 4 && e.target.status === 200){
+//     const data = JSON.parse(e.target.responseText)
+//       console.log(data)
+//   }else if (e.target.readyState === 4 ){
+//     console.log('errors!!!')
+//   }
+// })
+
+// request.open('GET', "http://puzzle.mead.io/puzzle?wordCount=3")
+// request.send()
+
+// http://restcountries.eu/rest/v2/all
+
+const countryCode = 'US'
+
 const request = new XMLHttpRequest()
-request.addEventListener("readystatechange", (e) => {
-  if(e.target.readyState === 4){
+request.addEventListener('readystatechange', (e) => {
+  if(e.target.readyState === 4 && e.target.status === 200){
     const data = JSON.parse(e.target.responseText)
-      console.log(data)
-  }
+    console.log(data)
+  } else if (e.target.readyState === 4 ){
+         console.log('errors!!!')
+       }
 })
 
-request.open('GET', "http://puzzle.mead.io/puzzle")
-request.send()
 
+
+request.open('GET', 'http://restcountries.eu/rest/v2/all')
+request.send()
