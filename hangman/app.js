0701e08bf4b5094cd1024a96ff6f9f4b5874f97f
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-30 18:01:44 -0400

    Add fetches

--- hangman/app.js
+++ hangman/app.js
@@ -20,34 +20,34 @@ guessesDOM.textContent = gameOne.statusMessage
 })
 
 // make http request 
-// const request = new XMLHttpRequest()
-// request.addEventListener("readystatechange", (e) => {
-//   if(e.target.readyState === 4 && e.target.status === 200){
-//     const data = JSON.parse(e.target.responseText)
-//       console.log(data)
-//   }else if (e.target.readyState === 4 ){
-//     console.log('errors!!!')
-//   }
-// })
+const request = new XMLHttpRequest()
+request.addEventListener("readystatechange", (e) => {
+  if(e.target.readyState === 4 && e.target.status === 200){
+    const data = JSON.parse(e.target.responseText)
+      console.log(data)
+  }else if (e.target.readyState === 4 ){
+    console.log('errors!!!')
+  }
+})
 
-// request.open('GET', "http://puzzle.mead.io/puzzle?wordCount=3")
-// request.send()
+request.open('GET', "http://puzzle.mead.io/puzzle?wordCount=3")
+request.send()
 
 // http://restcountries.eu/rest/v2/all
 
-const countryCode = 'US'
+// const countryCode = 'US'
 
-const request = new XMLHttpRequest()
-request.addEventListener('readystatechange', (e) => {
-  if(e.target.readyState === 4 && e.target.status === 200){
-    const data = JSON.parse(e.target.responseText)
-    console.log(data)
-  } else if (e.target.readyState === 4 ){
-         console.log('errors!!!')
-       }
-})
+// const request = new XMLHttpRequest()
+// request.addEventListener('readystatechange', (e) => {
+//   if(e.target.readyState === 4 && e.target.status === 200){
+//     const data = JSON.parse(e.target.responseText)
+//     const country = data.find((country) => country.alpha2Code === countryCode)
+//     console.log(country.name)
 
+//   } else if (e.target.readyState === 4 ){
+//          console.log('errors!!!')
+//        }
+// })
 
-
-request.open('GET', 'http://restcountries.eu/rest/v2/all')
-request.send()
+// request.open('GET', 'http://restcountries.eu/rest/v2/all')
+// request.send()
