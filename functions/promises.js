601edecc1c15280005becabe888aa52cf655fe18
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-10-31 20:19:30 -0400

    add promises

--- functions/promises.js
+++ functions/promises.js
@@ -1,6 +1,35 @@
+// callback
 const getDataCallback = (callback) => {
   setTimeout(() => {
     callback(undefined, "This is the data")
   }, 2000)
 }
 
+getDataCallback((error, data) => {
+  if(error){
+    console.log(error)
+  }else {
+    console.log(data)
+  }
+})
+
+//promise 
+
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {  
+   //resolve("this is the promise data")
+   reject("This is a promise error")
+  }, 2000)
+})
+
+myPromise.then((data) => {
+  console.log(data)
+}, (err) => {
+  console.log(err)
+})
+
+myPromise.then((data) => {
+  console.log(data)
+}, (err) => {
+  console.log(err)
+})
