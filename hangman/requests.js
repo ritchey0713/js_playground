9ba2f3d38193043be0170791361dd6e109e34520
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-11-01 13:56:06 -0400

    add calls

--- hangman/requests.js
+++ hangman/requests.js
@@ -1,34 +1,36 @@
-const getPuzzle = (wordCount, callback) => {
+const getPuzzle = (wordCount) => new Promise((resolve, reject) => {
   const request = new XMLHttpRequest()
 
   request.addEventListener("readystatechange", (e) => {
     if(e.target.readyState === 4 && e.target.status === 200){
       const data = JSON.parse(e.target.responseText)
-      callback(undefined, data.puzzle)
+      resolve(data.puzzle)
     }else if (e.target.readyState === 4 ){
-      callback("An error has taken place", undefined)
+      reject("An error has taken place")
     }
   })
   
   request.open('GET', `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
   request.send()
-}
+})
 
-const getCountry = (countryCode, callback) => {
+const getCountry = (countryCode) => new Promise((resolve, reject) => {
+  const request = new XMLHttpRequest()
 
-
-const request = new XMLHttpRequest()
-request.addEventListener('readystatechange', (e) => {
-  if(e.target.readyState === 4 && e.target.status === 200){
-    const data = JSON.parse(e.target.responseText)
-    const country = data.find((country) => country.alpha2Code === countryCode)
-    callback(undefined, country)   
+  request.addEventListener('readystatechange', (e) => {
+    if(e.target.readyState === 4 && e.target.status === 200){
+      const data = JSON.parse(e.target.responseText)
+      const country = data.find((country) => country.alpha2Code === countryCode)
+      resolve(country)   
 
   } else if (e.target.readyState === 4 ){
-         callback('errors!!!', undefined)
+         reject(`Errors have occured`)
        }
 })
 
 request.open('GET', 'http://restcountries.eu/rest/v2/all')
 request.send()
-}
+})
+
+
+
