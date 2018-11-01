63a45484dcf6e7af4b3ae484ed27152c2048ccd3
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-11-01 18:53:02 -0400

    add fetch

--- hangman/requests.js
+++ hangman/requests.js
@@ -1,18 +1,40 @@
-const getPuzzle = (wordCount) => new Promise((resolve, reject) => {
-  const request = new XMLHttpRequest()
-
-  request.addEventListener("readystatechange", (e) => {
-    if(e.target.readyState === 4 && e.target.status === 200){
-      const data = JSON.parse(e.target.responseText)
-      resolve(data.puzzle)
-    }else if (e.target.readyState === 4 ){
-      reject("An error has taken place")
-    }
+const getPuzzle = (wordCount) => {
+ return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`, {}).then((resp) => {
+      if(resp.status === 200){
+        return resp.json()
+      } else {
+        throw new Error("failed to fetch puzzle")
+      }
   })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const getPuzzle = (wordCount) => new Promise((resolve, reject) => {
+//   const request = new XMLHttpRequest()
+
+//   request.addEventListener("readystatechange", (e) => {
+//     if(e.target.readyState === 4 && e.target.status === 200){
+//       const data = JSON.parse(e.target.responseText)
+//       resolve(data.puzzle)
+//     }else if (e.target.readyState === 4 ){
+//       reject("An error has taken place")
+//     }
+//   })
   
-  request.open('GET', `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
-  request.send()
-})
+//   request.open('GET', `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+//   request.send()
+// })
 
 const getCountry = (countryCode) => new Promise((resolve, reject) => {
   const request = new XMLHttpRequest()
