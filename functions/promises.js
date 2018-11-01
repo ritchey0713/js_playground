dac3500989df7707a643b9e478e89ff982df8f5b
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-11-01 17:01:56 -0400

    add chains

--- functions/promises.js
+++ functions/promises.js
@@ -1,38 +1,73 @@
 // callback
-const getDataCallback = (callback) => {
+const getDataCallback = (num, callback) => {
   setTimeout(() => {
-    callback(undefined, "This is the data")
+    if(typeof num === 'number'){
+      callback(undefined, num * 2)
+    }else {
+      callback("number must be a number")
+    }
   }, 2000)
 }
 
-getDataCallback((error, data) => {
+getDataCallback(2, (error, data) => {
   if(error){
     console.log(error)
   }else {
-    console.log(data)
+    getDataCallback(data, (err, data) => {
+      if(err){
+        console.log(err)
+      }else{
+        console.log(data)
+      }
+    })
   }
 })
 
 //promise 
 
-const getDataPromise = (data) => new Promise((resolve, reject) => {
+const getDataPromise = (num) => new Promise((resolve, reject) => {
     setTimeout(() => {  
-     resolve(`success data: ${data}`)
-    //  reject("This is a promise error")
+     typeof num === "number" ? resolve(num * 2) : reject("num must be a number")
     }, 2000)
   })
 
 
-const myPromise = getDataPromise(123)
-
-myPromise.then((data) => {
-  console.log(data)
+getDataPromise(2).then((data) => {
+  getDataPromise(data).then((data) => {
+    console.log(`promise data: ${data}`)
+  }, (err) => {
+    console.log(err)
+  })
 }, (err) => {
   console.log(err)
 })
 
-myPromise.then((data) => {
+
+getDataPromise(10).then((data) => {
+    return getDataPromise(data)
+}).then((data) => {
   console.log(data)
-}, (err) => {
+}).catch((err) => {
   console.log(err)
 })
+
+
+
+
+
+
+
+
+// const myPromise = getDataPromise(123)
+
+// myPromise.then((data) => {
+//   console.log(data)
+// }, (err) => {
+//   console.log(err)
+// })
+
+// myPromise.then((data) => {
+//   console.log(data)
+// }, (err) => {
+//   console.log(err)
+// })
