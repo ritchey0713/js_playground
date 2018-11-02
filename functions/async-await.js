7f76aee10e3311114e764bcb70fa4cfeb9335e26
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-11-02 15:50:37 -0400

    add stacked call

--- functions/async-await.js
+++ functions/async-await.js
@@ -1,5 +1,29 @@
-const processData = async () => {
+// const processData = async () => {
+//   throw new Error("bad request!")
+//   return 12
+// }
 
+// processData().then((data) => {
+//   console.log('data', data)
+// }).catch((err) => {
+//   console.log('err', err)
+// })
+
+const getDataPromise = (num) => new Promise((resolve, reject) => {
+  setTimeout(() => {  
+   typeof num === "number" ? resolve(num * 2) : reject("num must be a number")
+  }, 2000)
+})
+
+const processData = async () => {
+  let data = await getDataPromise(2)
+  data = await getDataPromise(data)
+  data = await getDataPromise(data)
+  return data
 }
 
-console.log(processData())
+processData().then((data) => {
+  console.log('data', data)
+}).catch((err) => {
+  console.log('err', err)
+})
