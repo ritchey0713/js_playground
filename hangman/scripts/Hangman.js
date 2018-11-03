c9e9546690da825e27488d9c1942fe27e0969217
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-11-02 20:17:06 -0400

    move file

---
+++ hangman/scripts/Hangman.js
@@ -0,0 +1,148 @@
+class Hangman {
+  constructor(word, guessesLeft){
+    this.word = word.toLowerCase().split("")
+    this.guessesLeft = guessesLeft
+    this.guessedLetters = []
+    this.status = "playing"
+  }
+  get statusMessage(){
+    if(this.status === "playing"){
+      return `Guesses left ${this.guessesLeft}`
+    } else if (this.status === "failed"){
+      return `Nice try! the word was "${this.word.join("")}".`
+    }else{
+      return `Great work! You guess the word!`
+    }
+  }
+  //change to get 
+  getStatus(){
+    const finished = this.word.every((letter) => this.guessedLetters.includes(letter) || letter === " ")
+    if(this.guessesLeft === 0){
+      this.status = "failed" 
+    } else if (finished){
+      this.status = "finished"
+    }else {
+      this.status = 'playing'
+    }
+  }
+
+  //change to get
+  get puzzle(){
+    let puzzle = ""
+    this.word.forEach(letter => {
+      if(this.guessedLetters.includes(letter) || letter === " "){
+        puzzle += letter
+      }else{
+        puzzle += "*"
+      }
+    })
+  
+      return puzzle
+  }
+  makeGuess(guess){
+    guess = guess.toLowerCase()
+    const isUnique = !this.guessedLetters.includes(guess)
+    const isBadGuess = !this.word.includes(guess)
+      if(this.status != "playing"){
+        return
+      }
+      if(isUnique){
+        this.guessedLetters.push(guess)
+      } 
+      if(isUnique && isBadGuess){
+        this.guessesLeft--
+      }
+      this.getStatus()
+    } 
+}
+
+
+// const Hangman = function(word, guessesLeft, guessedLetters){
+//   this.word = word.toLowerCase().split("")
+//   this.guessesLeft = guessesLeft
+//   this.guessedLetters = []
+//   this.status = "playing"
+// }
+
+// Hangman.prototype.getStatus = function(){
+//   let newWord = []
+//   this.word.forEach((letter) => {
+//     if (this.guessedLetters.includes(letter)){
+//       newWord.push(letter)
+//     }
+//     if (newWord.toString() === this.word.toString)
+//     this.status = 'Finished'
+//   })
+//   if(this.guessesLeft < 1){
+//     this.status = "Failed" 
+//   } 
+// }
+
+// Hangman.prototype.statusMessage = function(){
+//     if(this.status === "playing"){
+//       return `Guesses left ${this.guessesLeft}`
+//     } else if (this.status === "failed"){
+//       return `Nice try! the word was "${this.word.join("")}".`
+//     }else{
+//       return `Great work! You guess the word!`
+//     }
+// }
+
+// Hangman.prototype.getStatus = function(){
+// const finished = this.word.every((letter) => this.guessedLetters.includes(letter))
+
+//sol 2 
+  //  const lettersUnguessed = this.word.filter((letter) => {
+//    return !this.guessedLetters.includes(letter)
+//  })
+//  const finished = lettersUnguessed.length === 0 
+ 
+ //sol 1 
+  // let finished = true 
+
+  // this.word.forEach((letter) => {
+  //   if(this.guessedLetters.includes(letter)){
+  //   }else{
+  //     finished = false
+  //   }
+  // })
+  
+//  if(this.guessesLeft === 0){
+//     this.status = "failed" 
+//   } else if (finished){
+//     this.status = "finished"
+//   }else {
+//     this.status = 'playing'
+//   }
+  
+//}
+
+// Hangman.prototype.getPuzzle = function (){
+//   let puzzle = ""
+//   this.word.forEach(letter => {
+//     if(this.guessedLetters.includes(letter)){
+//       puzzle += letter
+//     }else{
+//       puzzle += "*"
+//     }
+//   })
+
+//     return puzzle
+// }
+
+// Hangman.prototype.makeGuess = function(guess){
+// guess = guess.toLowerCase()
+// const isUnique = !this.guessedLetters.includes(guess)
+// const isBadGuess = !this.word.includes(guess)
+//   if(this.status != "playing"){
+//     return
+//   }
+//   if(isUnique){
+//     this.guessedLetters.push(guess)
+//   } 
+//   if(isUnique && isBadGuess){
+//     this.guessesLeft--
+//   }
+//   this.getStatus()
+// } 
+
---
+++ hangman/scripts/app.js
@@ -0,0 +1,100 @@
+const gameWord = document.querySelector('#game-word')
+const guessesDOM = document.querySelector("#guesses-left")
+let gameOne
+
+window.addEventListener('keypress', (e) => {
+  const guess = String.fromCharCode(e.charCode)
+  gameOne.makeGuess(guess)
+  render()
+})
+
+const render = () => {
+  gameWord.textContent = gameOne.puzzle
+  guessesDOM.textContent = gameOne.statusMessage
+}
+
+const startGame = async () => {
+  const puzzle = await getPuzzle('2')
+  gameOne = new Hangman(puzzle, 5)
+  render()
+}
+
+document.querySelector('#reset').addEventListener('click', startGame)
+
+startGame()
+
+
+// getPuzzle("2").then((puzzle) => {
+//   console.log(puzzle)
+// }).catch((err) => {
+//   console.log(err)
+// })
+
+
+
+// getLocation().then((location) => {
+//   return getCountry(location.country)
+// }).then((country) => {
+//   console.log(country)
+// }).catch((err) => {
+//   console.log(err)
+// })
+
+// getCurrentCountry().then((country) => {
+//   console.log(country)
+// }).catch((err) => {
+//   console.log(err)
+// })
+
+
+// getPuzzle("2").then((puzzle) => {
+//   console.log(puzzle)
+// }, (error) => {
+//   console.log(`error: ${error}`)
+// })
+
+// const countryCode = 'US'
+
+// getCountry(countryCode).then((country) => {
+//   console.log(country.name)
+// }, (error) => {
+//   console.log(`ERROR: ${error}`)
+// })
+
+// make http request 
+
+
+// http://restcountries.eu/rest/v2/all
+
+// const countryCode = 'US'
+
+// const request = new XMLHttpRequest()
+// request.addEventListener('readystatechange', (e) => {
+//   if(e.target.readyState === 4 && e.target.status === 200){
+//     const data = JSON.parse(e.target.responseText)
+//     const country = data.find((country) => country.alpha2Code === countryCode)
+//     console.log(country.name)
+
+//   } else if (e.target.readyState === 4 ){
+//          console.log('errors!!!')
+//        }
+// })
+
+// request.open('GET', 'http://restcountries.eu/rest/v2/all')
+// request.send()
+
+// fetch('http://puzzle.mead.io/puzzle', {}).then((resp) => {
+//   if(resp.status === 200){
+//     return resp.json()
+//   }else{
+//      throw new Error("unable to fetch")
+//   }
+// }).then((data) => {
+//   console.log(data.puzzle)
+// }).catch((err)=> {
+//   console.log(err)
+// })
+
+
+// http://ipinfo.io/json?token=010974ce661a1a
+
