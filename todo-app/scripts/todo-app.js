b1bf564143f860ce09b51f49d9ba6e686bf30f39
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-11-03 21:00:21 -0400

    add classes

--- todo-app/scripts/todo-app.js
+++ todo-app/scripts/todo-app.js
@@ -16,15 +16,18 @@ document.querySelector('#search-text').addEventListener('input', (e) => {
 
 document.querySelector('#add-todo').addEventListener('submit', e => {
   e.preventDefault()
-  toDoName = e.target.elements.addToDo.value
+  let text = e.target.elements.text.value.trim()
+  console.log()
+  if(text.length > 0){
   todos.push( { 
     id: uuidv4(),
-    text: toDoName,
+    text,
     completed: false
   })
     saveTodos(todos)
-   e.target.elements.addToDo.value = ""
+   e.target.elements.text.value = ""
     renderTodos(todos, filters)
+  }
 })
 
 document.querySelector('#hide-complete').addEventListener('change', (e) => {
